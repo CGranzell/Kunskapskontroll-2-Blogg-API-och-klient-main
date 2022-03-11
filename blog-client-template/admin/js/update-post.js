c44ec840b85ffbecad9dde730a77cd76a8576af6
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-03-11 03:44:15 +0100

    changes

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -1,13 +1,14 @@
-//window.onload = function() {
+window.onload = function() {
     const queryString = window.location.search; //  ?id=6229f059f1d1df664039fdd1 (olika ID beroende på vilket inlägg man trycker på)
     
     const urlParams = new URLSearchParams(queryString);  // metod som används för att arbeta med querystrings
     console.log(urlParams);
-    // fetchPun(urlParams.get(id));  //Nu kickar du igång funktionen
+    console.log(urlParams.get('id'));
+    
    
-    fetchPun(urlParams);
-    updatePunEvent(urlParams);
-
+    fetchPun(urlParams); //Nu kickar du igång funktionen
+    //updatePunEvent(urlParams);
+}
 
 
 
@@ -15,16 +16,18 @@ async function fetchPun(urlParams) {
     try {
         const response = await fetch(`http://localhost:5000/posts/${urlParams.get('id')}`)  //ID för inlägget är: 6229f059f1d1df664039fdd1
         const post = await response.json();
+        console.log(post);
         
                 
-        console.log(document.getElementById('content-textarea').innerText = post.content);
-            
+        console.log(document.getElementById('content').innerText = post.content);
+       console.log(document.getElementById('author').innerText = post.author);  //Funkar ej
+        console.log(document.getElementById('title').innerText = post.title);  //Funkar ej
       
     } catch(error) {
         console.log(error)
     }
     
-}
+
 
    function updatePunEvent(urlParams) {
     const form = document.getElementById(update-post);
@@ -35,10 +38,14 @@ async function fetchPun(urlParams) {
         const formData = new FormData(e.target) // e.target is the form, in this case
         console.log(formData);
         const JSONString = {
-            content: formData.get('content') //content stod här innan
+            content: formData.get('content')
+            
+            
+            
         };
         console.log(JSON.stringify(JSONString));
-
+        
+        
         try {
             const response = await fetch(`http://localhost:5000/posts/${urlParams.get('id')}`, {
                 method: 'PATCH', // GET/POST/PATCH/DELETE
@@ -46,6 +53,8 @@ async function fetchPun(urlParams) {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(JSONString),
+
+                
             })
 
             if (!response.ok) {
@@ -53,10 +62,11 @@ async function fetchPun(urlParams) {
             }
 
             window.location.replace('index.html') // redirects to index.html
+
+
         } catch(error) {
             console.log(error);
         }
     })
 }
-
-  
+}
